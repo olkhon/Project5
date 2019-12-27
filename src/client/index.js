38c82ee22f6dc48488f8437c3ea4530a5a5d823c
olkhon <alexander.stapel@gmail.com>
2019-12-27 14:20:05 +0300

    initialize dotenv in server.js

--- src/client/index.js
+++ src/client/index.js
@@ -4,7 +4,3 @@ import { nameChecker } from './js/nameChecker'
 import './styles/resets.scss'
 
 alert('This is my main js file!')
-
-
-alert('test')
-alert('auto')
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,14 @@
+// initialize dotenv
+
+const dotenv = require('dotenv');
+dotenv.config();
+
 // initialize aylien
 
 var aylien = require('aylien_textapi');
 var textapi = new AYLIENTextAPI({
-    application_id: "your-api-id",
-    application_key: "your-api-key"
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
 });
 
 
