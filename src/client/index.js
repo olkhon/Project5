9c4d94734699b17453c57728ac5f2e9a595bf4c5
olkhon <alexander.stapel@gmail.com>
2019-12-28 19:45:46 +0300

    add other imports styles

--- src/client/index.js
+++ src/client/index.js
@@ -2,5 +2,10 @@ import { handleSubmit } from './js/formHandler'
 import { nameChecker } from './js/nameChecker'
 
 import './styles/resets.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+import './styles/base.scss'
+
 
 alert('This is my main js file!')
