715a87cecd208459bcbc325096d382352424a969
olkhon <alexander.stapel@gmail.com>
2020-01-09 20:56:34 +0300

    post function client side formHandler.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,22 +1,31 @@
-function handleSubmit(event) {
+async function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+    let formText = document.getElementById("name").value
+    const userInput = document.getElementById("results")
 
 
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:3000/aylien')
-        .then(res => res.json())
-        .then(function(res) {
-            document.getElementById('results').innerHTML = res.message
-        })
+    const dataInput = {
+        url: formText
+    }
 
-
-
+    const res = await postData("/sentiment", dataInput);
+    userInput.textContent = `Textpolarity is ${res.msg1} and Textpolarityconfidence is  ${res.msg2}`;
 
 }
+
+async function postData(url = "", data = {}) {
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+    return await response.json();
+}
+
 
 export { handleSubmit }
