fe60748e36325f024af41e021d1ced5dd68f68b6
olkhon <alexander.stapel@gmail.com>
2020-01-09 21:01:37 +0300

    post function for api call, aylien initialize, bode parser middleware

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,37 @@
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+const bodyParser = require('body-parser')
+
+
+
+
+const app = express()
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+app.get('/', function(req, res) {
+    res.sendFile('dist/index.html')
+})
+
+app.get('/test', function(req, res) {
+    res.send(mockAPIResponse)
+})
+
+//Configuring express to use body-parser as middle-ware.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// designates what port the app will listen to for incoming requests
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!')
+})
+
 // initialize dotenv
 
 const dotenv = require('dotenv');
@@ -11,34 +45,28 @@ var textapi = new AYLIENTextAPI({
     application_key: process.env.API_KEY
 });
 
+app.post("/sentiment", function(req, res) {
+    const parseUrl = req.body.url;
 
 
+    if (textapi) {
+        textapi.sentiment({
+                url: parseUrl
+            },
+            (err, resp) => {
+                if (err === null) {
+                    res.json({
+                        msg1: resp.polarity,
+                        msg2: resp.polarity_confidence
+                    });
+                } else {
+                    const failedText = "Something went wrong!"
+                    res.json({
+                        message: failedText
+                    })
+                }
+            }
+        )
+    }
 
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
-const aylienAPIResponse = require('./aylienAPI.js')
-
-const app = express()
-
-app.use(express.static('dist'))
-
-console.log(__dirname)
-
-app.get('/', function(req, res) {
-    res.sendFile('dist/index.html')
-})
-
-
-app.get('/test', function(req, res) {
-    res.send(mockAPIResponse)
-})
-
-app.get('/aylien', function(req, res) {
-    res.send(aylienAPIResponse)
-})
-
-// designates what port the app will listen to for incoming requests
-app.listen(3000, function() {
-    console.log('Example app listening on port 3000!')
-})
+});
