8fd80f01131eadefb7169e90560a89db2307caf9
olkhon <alexander.stapel@gmail.com>
2020-01-12 14:09:10 +0300

    scss bug webpack config prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,10 +9,15 @@ module.exports = {
 
     module: {
         rules: [{
-            test: '/\.js$/',
-            exclude: '/node_modules/',
-            loader: 'babel-loader'
-        }]
+                test: '/\.js$/',
+                exclude: '/node_modules/',
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
     },
     plugins: [
         new HtmlWebPackPlugin({
